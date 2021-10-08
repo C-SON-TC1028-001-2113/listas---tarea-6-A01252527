ebddb6e42c5c44c5e3f0438dae43d3ca31e6f423
A01252527 <a01252527@itesm.mx>
2021-10-08 05:47:11 +0000

    d

--- assignments/09SerieFibonacciListas/src/exercise.py
+++ assignments/09SerieFibonacciListas/src/exercise.py
@@ -1,6 +1,30 @@
 def main():
-    
-    pass
+    a = -1
+    x = 1
+    y = 1
+    s = 0
+    lista = []
+    while a <= -1 :
+        a = int(input())
+    if a == 0 :
+        print(lista)
+    elif a==1 :
+        lista.append(0)
+        print(lista)
+    elif a==2 :
+        lista.append(0)
+        lista.append(1)
+        print(lista)
+    else :
+        lista.append(0)
+        lista.append(1)
+        lista.append(1)
+        for i in range(a-3) :
+            s = x + y
+            y = x
+            x = s
+            lista.append(s)
+        print(lista)
 
 if __name__=='__main__':
     main()
